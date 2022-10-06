96b8167714427e7622deb821185c9e9f7e236f24
BuildTools <22253494@student.uwa.edu.au>
2022-10-07 00:23:52 +0800

    init

--- controller/loginController.js
+++ controller/loginController.js
@@ -2,10 +2,19 @@
 
 const loginGet = ("/login", (req, res) => {
     res.render('login', {
-        title: 'BarBox | Login',
+        title: 'Footy Coach | Login',
         logged: req.session.loggedIn,
         username: req.session.username
     });
+    return;
+});
+
+const registerPost = ("/api/v1/register", (req, res) => {
+
+    //? Take in rego form details
+    //? Hash password
+    //? Store in DB
+
 });
 
 module.exports = {
